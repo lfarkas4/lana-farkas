82eb0ba8272ea24053bbd146704635898374298f
lfarkas4 <lfarkas@andrew.cmu.edu>
2025-12-11 14:06:25 -0500

    placeholder watch thumb

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -45,7 +45,7 @@ export const caseStudies = [
       description:
         "Conceptualized a wearable experience to support patients through cancer care at UPMC Magee-Womens Hospital.",
       link: "/case-studies/hira",
-      image: "/assets/watchmock.svg",
+      image: "/assets/watchmock1.png",
       brandMark: "/assets/hira-mark.svg",
       brandMarkH: "45px",                // ← per-project size
       meta: {
