34bab91259fda352af763aee321cb3ffd45ae2d3
lfarkas4 <lfarkas@andrew.cmu.edu>
2025-12-17 03:21:55 -0500

    OK NYOW IM DONE BAIIIIII

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -45,7 +45,7 @@ export const caseStudies = [
       description:
         "Conceptualized a wearable experience to support patients through cancer care at UPMC Magee-Womens Hospital.",
       link: "/case-studies/hira",
-      video: "/assets/hiyasss.mp4",
+      video: "/assets/hiyas.mp4",
       brandMark: "/assets/hira-mark.svg",
       brandMarkH: "45px",                // ← per-project size
       meta: {
