af9b6a94e4e7a2ba073476720e88e4c54634251d
lfarkas4 <lfarkas@andrew.cmu.edu>
2025-12-18 20:00:58 -0500

    vercel analytics yayyyyy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/next";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 import Navigation from "./components/Navbar";
@@ -24,6 +25,7 @@ function AppContent() {
 
   return (
     <>
+      <Analytics />
       <CosmicBackground />
       {!isDetailPage && <Navigation />}
       <Routes>
