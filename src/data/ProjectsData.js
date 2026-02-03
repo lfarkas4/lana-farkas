f4e3b50e862a2d86957fef8c73b7eb70bac77ec2
lfarkas4 <lfarkas@andrew.cmu.edu>
2026-02-03 08:40:36 -0500

    solution for now

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -96,7 +96,7 @@ export const caseStudies = [
       description: "Developed interactive game prototype using Arduino and digital fabrication tools.",
       link: "/case-studies/taptap",
       image: "/assets/taptap2.png",
-      brandMark: "/assets/taptap-markk.svg",
+      brandMark: "/assets/tech.png",
       brandMarkH: "24px",
       meta: {
         role: "Hardware Design & Code Integration",
